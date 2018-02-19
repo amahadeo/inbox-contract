019b197b9474298c966c73703f0b7bdbca671099
Anthony Mahadeo <anthony@financialapps.com>
2018-02-19 15:42:40 -0500

    Add tests for Inbox contract

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,11 +1,13 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3'); // we capitalize constructor function
-const web3 = new Web3(ganache.provider()); // initialize instance w/ provider
+const provider = ganache.provider();
+const web3 = new Web3(provider); // initialize instance w/ provider
 const { interface, bytecode } = require('../compile')
 
 let accounts;
 let inbox;
+const INITIAL_STRING = 'Hi there!'
 
 beforeEach(async () => {
   // Get a list of all accounts
@@ -16,15 +18,28 @@ beforeEach(async () => {
   accounts = await web3.eth.getAccounts(); // async/await alternative to 'then'
 
   // Use one off those accounts to deploy the contracts
-  inbox = await new web3.eth.Contract(JSON.parse(interface)) // interface is our ABI, bytecode is contract bytecode
-    .deploy({ data: bytecode, arguments: ['Hi there!'] }) // arguments = Inbox constructor arguments
-    .send({ from: accounts[0], gas: '1000000' }) // account to send transaction from (already supplied and unlocked by ganache)
+  inbox = await new web3.eth.Contract(JSON.parse(interface)) // interface is our ABI, bytecode is contract bytecode // instance of Contract constructor
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] }) // arguments = Inbox constructor arguments // deploy initializes contract
+    .send({ from: accounts[0], gas: '1000000' }) // account to send transaction from (already supplied and unlocked by ganache) // send actually sends/deploys contract to network
+
+  inbox.setProvider(provider); // addition because of breaking build
 });
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
   });
+  // 2 types => Calling a function (retrieving contract data, free), Sending a transaction to a function (changing contract data, costs money)
+  it('has a default message', async () => { // calling a function/method on the contract is relatively quick but still async
+    const message = await inbox.methods.message().call() // first set of arguments are sent to contract respective function, second set customize how function gets called
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change the message', async () => { // changing contract data means sending a transaction to a function, this costs money & needs sender, this returns transaction hash
+    await inbox.methods.setMessage('bye').send({ from: accounts[0] })
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'bye')
+  })
 });
 // class Car {
 //   park() {
